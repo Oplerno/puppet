d5170052897f0b4c4834db8c5a0db8b7421750e0
webhat <daniel.crompton@gmail.com>
2014-03-14 16:33:08 +0000

    kill and disable turnkey tools

--- classes/apache2.pp
+++ classes/apache2.pp
@@ -1,6 +1,22 @@
 ## apache2.pp
 
 service { "apache2":
-  ensure => "stopped",
+	ensure => "stopped",
+	enable => false,
+}
+
+service { "shellinabox":
+	ensure => "stopped",
+	enable => false,
+}
+
+service { "webmin":
+	ensure => "stopped",
+	enable => false,
+}
+
+service { "confconsole":
+	ensure => "stopped",
+	enable => false,
 }
 
