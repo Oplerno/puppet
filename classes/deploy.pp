56128c64fbe63fb46802805c6bffb065b21d7359
webhat <daniel.crompton@gmail.com>
2014-03-16 23:43:00 +0000

    add rvm for user deploy

--- classes/deploy.pp
+++ classes/deploy.pp
@@ -1,7 +1,21 @@
 ## deploy
 
 user { 'deploy':
+  ensure => 'present',
   name   => 'deploy',
   shell  => '/bin/bash',
-  ensure => 'present',
+}
+
+file {'/home/deploy/.ssh':
+  ensure => directory,
+  owner  => 'deploy',
+  mode   => '0600',
+}
+
+file { 'rvmrc':
+  ensure  => present,
+  content => inline_template('#!/bin/bash\r[[ -s "/usr/local/rvm/scripts/rvm" ]] && source "/usr/local/rvm/scripts/rvm"'),
+  name    => '/home/deploy/.bashrc.d/rvmrc',
+  owner   => 'deploy',
+  mode    => '0600',
 }
