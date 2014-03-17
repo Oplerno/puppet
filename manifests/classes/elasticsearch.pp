2727d43f23592b00223542d1e3e28dead27616f8
webhat <daniel.crompton@gmail.com>
2014-03-17 21:41:37 +0000

    elasticsearch config

---
+++ manifests/classes/elasticsearch.pp
@@ -0,0 +1,30 @@
+## elasticsearch.pp
+
+apt::source { 'elasticsearch':
+  location          => 'http://packages.elasticsearch.org/elasticsearch/1.0/debian',
+  release           => 'stable',
+  repos             => 'main',
+  before            => Class['elasticsearch'],
+  require           => Package['openjdk-7-jre-headless'],
+}
+
+package { 'openjdk-7-jre-headless':
+  ensure => 'installed',
+}
+
+class { 'elasticsearch':
+  version                  => '1.0.1',
+  config                   => {
+    'node'                 => {
+      'name'               => 'elasticsearch001'
+    },
+    'index'                => {
+      'number_of_replicas' => '0',
+      'number_of_shards'   => '5'
+    },
+    'network'              => {
+      'host'               => $::ipaddress
+    }
+  }
+}
+
