99fa41ddfab4611007a4ff883a2eb28a7ff22ac1
Daniël W. Crompton <daniel.crompton@gmail.com>
2014-06-23 20:32:39 +0200

    dependencies for config file

--- manifests/classes/sidekiq.pp
+++ manifests/classes/sidekiq.pp
@@ -13,11 +13,18 @@ class sidekiq {
     mode    => '0700',
     content => template('sidekiq/sidekiq.erb'),
     before  => Service['sidekiq'],
-  	owner   => 'root',
-  	group   => 'root',
+    owner   => 'root',
+    group   => 'root',
+    require => File['/etc/profile.d/sidekiq.sh'],
   }
 
 	file { '/etc/profile.d/sidekiq.sh':
+    notify  => Service['sidekiq'],
+    before  => Service['sidekiq'],
+    ensure  => 'present',
+    mode    => '0700',
+    owner   => 'root',
+    group   => 'root',
     content => template('sidekiq/sidekiq.sh.erb')
   }
 }
